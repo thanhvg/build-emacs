9feb8fd5abe91a77293ecc3f40bb585dc4f72303
Thanh Vuong <thanhvg@gmail.com>
2020-09-01 17:56:54 -0600

    enable bindir for emacs27 build

--- build-emacs27.sh
+++ build-emacs27.sh
@@ -2,6 +2,6 @@ cd ../emacs27
 git clean -fdx
 git pull origin emacs-27
 ./autogen.sh
-./configure --with-modules --with-json --prefix=/home/thanh/emacs27 
+./configure --with-modules --with-json --prefix=/home/thanh/emacs27 --bindir=/home/thanh/bin/
 make -j4
 make install
