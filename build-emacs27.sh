613479b7a41e5a0327b7ec5f2175cf7b335fdf67
Thanh Vuong <thanhvg@gmail.com>
2021-03-22 23:46:55 -0600

    add svg

--- build-emacs27.sh
+++ build-emacs27.sh
@@ -3,6 +3,6 @@ cd ../emacs27
 git clean -fdx
 git pull origin emacs-27
 ./autogen.sh
-./configure --with-modules --with-json --prefix=/home/thanh/emacs27 --bindir=/home/thanh/bin/
+./configure --with-modules --with-json --with-rsvg --prefix=/home/thanh/emacs27 --bindir=/home/thanh/bin/
 make -j4
 make install
--- build-gccemacs.sh
+++ build-gccemacs.sh
@@ -7,6 +7,6 @@ export LD_LIBRARY_PATH=/home/thanh/gcc/lib
 export LIBRARY_PATH=/home/thanh/gcc/lib
 echo $PATH
 ./autogen.sh
-./configure --with-nativecomp --with-modules --prefix=/home/thanh/gccemacs
+./configure --with-nativecomp --with-modules --with-rsvg --prefix=/home/thanh/gccemacs
 make -j4 
 make install
