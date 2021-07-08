c62fe448f5df25f09f07fda7b83ce59b94223bf9
Thanh Vuong <thanhvg@gmail.com>
2021-07-08 12:26:37 -0600

    update lucid build
    add package dependency

--- build-emacs27-lucid.sh
+++ build-emacs27-lucid.sh
@@ -1,8 +1,9 @@
+sudo apt install liblockfile-bin liblockfile1 libm17n-0 libotf0 m17n-db xaw3dg liblockfile-dev  libm17n-dev libotf-dev xaw3dg-dev 
 rm -r ~/emacs27-lucid
 cd ../emacs27-lucid
 git clean -fdx
 git pull origin emacs-27
 ./autogen.sh
-./configure --with-x-toolkit=lucid --with-modules --with-json --with-rsvg --prefix=/home/thanh/emacs27-lucid
+./configure --prefix=$HOME/emacs27lucid --with-x-toolkit=lucid --with-xft --without-gconf --without-gsettings --with-modules
 make -j4
 make install
