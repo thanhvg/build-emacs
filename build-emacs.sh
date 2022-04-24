1daf18228b0b30dc5b84123d1b755b9ff87b0329
Thanh Vuong <thanhvg@gmail.com>
2022-04-24 11:02:36 -0600

    22.04

--- build-emacs.sh
+++ build-emacs.sh
@@ -1,9 +1,10 @@
 cd ../emacs --with-x-toolkit=lucid
-rm -r ~/emacs --with-x-toolkit=lucid
+rm -r ~/emacs
 git clean -fdx
 git pull origin master
-export CC=/usr/bin/gcc-10 CXX=/usr/bin/gcc-10
+# enable this for 20.04
+# export CC=/usr/bin/gcc-10 CXX=/usr/bin/gcc-10
 ./autogen.sh
 ./configure --with-native-compilation --with-modules --with-rsvg --prefix=$HOME/emacs --with-x-toolkit=lucid
-make -j4 
+make -j4
 make install
--- build-emacs28.sh
+++ build-emacs28.sh
@@ -2,9 +2,28 @@ cd ../emacs28
 rm -r ~/emacs28
 git clean -fdx
 git pull
-export CC=/usr/bin/gcc-10 CXX=/usr/bin/gcc-10
+
+# enable this for 20.04
+# export CC=/usr/bin/gcc-10 CXX=/usr/bin/gcc-10
+
+read -p "Apply CC/CCX settings for 20.04 (y/n)?" gcc_choice
+if [ "$gcc_choice" = "y" ]; then
+    echo "applying gcc\n";
+    export CC=/usr/bin/gcc-10 CXX=/usr/bin/gcc-10
+else
+    echo "use system gcc\n";
+fi
+
 ./autogen.sh
 ./configure  --prefix=$HOME/emacs28 --with-native-compilation --with-x-toolkit=lucid --with-modules --with-rsvg
- 
-make -j4 
+
+read -p "Ready to install (y/n)?" ready_choice
+if [ "$ready_choice" = "y" ]; then
+    echo "Installing\n";
+else
+    echo "Exiting\n";
+    exit 1
+fi
+
+make -j4
 make install
