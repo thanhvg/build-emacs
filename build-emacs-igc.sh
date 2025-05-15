167dc06f96710e0d880603dabfc4c5124a730b50
Thanh <thanhvg@gmail.com>
2025-05-14 21:24:55 -0600

    improve

--- build-emacs-igc.sh
+++ build-emacs-igc.sh
@@ -1,18 +1,24 @@
-# git clone -b feature/igc --depth 1 https://git.savannah.gnu.org/git/emacs.git ~/git/emacsigc
-# rm -r ~/git/emacsigc
-# git clone -b feature/igc --depth 1 https://github.com/emacs-mirror/emacs ~/git/emacsigc
-cd ~/git/emacsigc
+read -p "First time build? (y/N)" ready_choice
+
+if [ "$ready_choice" = "y" ]; then
+    # git clone -b feature/igc --depth 1 https://git.savannah.gnu.org/git/emacs.git ~/git/emacsigc
+    git clone -b feature/igc --depth 1 https://github.com/emacs-mirror/emacs ~/git/emacsigc
+    cd ~/git/emacsigc
+else 
+    cd ~/git/emacsigc
+    git clean -fdx
+    # no pull of full history
+    git fetch --depth=1 origin feature/igc
+    # git pull 
+fi
+
 rm -r ~/emacsigc
-git clean -fdx
-# no pull of full history
-git fetch --depth=1 origin feature/igc
-# git pull 
 
 MPS_ARTIFACTS=~/libs/mps/mps4emacs
 
 ./autogen.sh
 
-read -p "Use gkt instead of lucid (y/N) " ready_choice
+read -p "Use gtk instead of lucid (y/N) " ready_choice
 
 if [ "$ready_choice" = "y" ]; then
     ./configure CPPFLAGS=-I$MPS_ARTIFACTS LDFLAGS=-L$MPS_ARTIFACTS --with-mps=yes --prefix=$HOME/emacsigc --with-native-compilation --with-modules --with-json --with-rsvg -with-tree-sitter --with-imagemagick --with-webp --without-compress-install
--- build-mps.sh
+++ build-mps.sh
@@ -1,8 +1,18 @@
-# git clone https://github.com/Ravenbrook/mps.git ~/git/mps
 
+read -p "First time build? (y/N)" ready_choice
+
+if [ "$ready_choice" = "y" ]; then
+    git clone --depth 1 https://github.com/Ravenbrook/mps.git ~/git/mps
+    cd ~/git/mps
+else 
+    cd ~/git/mps
+    git clean -fdx
+    # no pull of full history
+    git fetch --depth=1 origin master
+fi
 
 MPS_ARTIFACTS=~/libs/mps
-cd ../mps
+cd ~/git/mps
 autoconf
 ./configure --prefix="$MPS_ARTIFACTS"
 make CFLAGSCOMPILERSTRICT="-Wno-error"
